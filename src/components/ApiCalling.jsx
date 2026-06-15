1ab50e5ea276c5bac1ae78c97402554de44dd7da
Shashwat Gupta <shashwat@gmail.com>
2026-06-16 00:19:09 +0530

    Fixed swiggy api blocking in browser

--- src/components/ApiCalling.jsx
+++ src/components/ApiCalling.jsx
@@ -2,17 +2,32 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 function ApiCalling() {
-    let [allRestaurantArr,setAllRestaurantArr] = useState([])
-    useEffect(()=>{
-        async function calling(){
-            const API = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.99740&lng=79.00110&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    let [allRestaurantArr, setAllRestaurantArr] = useState([])
+useEffect(() => {
+    async function calling() {
+        try {
+            const SWIGGY_URL = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.99740&lng=79.00110&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+            const API = "https://corsproxy.io/?" + encodeURIComponent(SWIGGY_URL)
+
             let resp = await axios.get(API)
-            console.log(resp.data.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
-            setAllRestaurantArr(resp.data.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
+
+            // Safely find the card that has restaurants
+            const cards = resp.data.data.cards;
+            const restaurantCard = cards.find(
+                c => c?.card?.card?.gridElements?.infoWithStyle?.restaurants
+            );
+
+            const restaurants = restaurantCard?.card?.card?.gridElements?.infoWithStyle?.restaurants || [];
+            console.log(restaurants);
+            setAllRestaurantArr(restaurants);
+
+        } catch (err) {
+            console.error("Failed to fetch restaurants:", err);
         }
-        calling()
-    } , [])
-  return allRestaurantArr;
+    }
+    calling()
+}, [])
+    return allRestaurantArr;
 }
 
 export default ApiCalling
