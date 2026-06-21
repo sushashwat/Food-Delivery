1e3661b0a7619c8940987f97e5d9944dd4da8328
Shashwat Gupta <guptashashwat155@gmail.com>
2026-06-21 18:49:05 +0530

    Added dynamic find instead of hard coded cards

--- src/components/ApiCalling.jsx
+++ src/components/ApiCalling.jsx
@@ -7,8 +7,18 @@ function useApiCalling() {
         async function calling(){
             const API = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.99740&lng=79.00110&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
             let resp = await axios.get(API)
-            console.log(resp.data.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
-            setAllRestaurantArr(resp.data.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
+
+            // Search through all cards to find the one containing restaurant data
+            const restaurantCard = resp.data.data.cards.find(
+                (card) => card?.card?.card?.gridElements?.infoWithStyle?.restaurants
+            )
+
+            if (restaurantCard) {
+                console.log(restaurantCard.card.card.gridElements.infoWithStyle.restaurants)
+                setAllRestaurantArr(restaurantCard.card.card.gridElements.infoWithStyle.restaurants)
+            } else {
+                console.warn("Restaurant list card not found in API response")
+            }
         }
         calling()
     } , [])
